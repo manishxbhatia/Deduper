499f9d6a5361680cf99ea6601f10ab7458706e2c
Manish Bhatia <bhatia2703@gmail.com>
2025-02-02 22:07:50 +0530

    Added python script to parse ICICI Credit card transactions csv files so that they are easier to upload into gnucash.
    Changes to be committed:
    	modified:   README.md
    	new file:   format_icici_cc.py

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Deduper
+# Format ICICI credit card transactions csv file to make it easy to upload into gnucash
+#
---
+++ format_icici_cc.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import os
+import time
+import argparse
+import csv
+import re
+
+def ParseAmount(amount):
+    x = re.sub(",", "", amount).split()
+    if x[1] == "Cr.":
+        print("Got credit entry [%s] [%s]" % (x[0], x[1]))
+        return -1*float(x[0])
+    else:
+        return x[0]
+
+def GetFields(csvreader):
+    values = next(csvreader)
+    fields = {}
+    for index, value in enumerate(values):
+        fields[value] = index
+    return fields
+
+def processInputFile(filename):
+    lines = []
+    skip = True
+    with open(filename, "r") as f:
+        for line in f:
+            if not skip:
+                lines.append(line.strip())
+            if line.startswith(",Transaction Details,,,,"):
+                skip = False
+    for line in lines:
+        print("Got line: %s" % line)
+    csvreader = csv.reader(lines)
+    fields = GetFields(csvreader)
+    processed_lines = []
+    processed_lines.append(("Transaction Date",
+                            "Details",
+                            "Amount (INR)",
+                            "Ref"))
+                            
+    for row in csvreader:
+        date = row[fields["Transaction Date"]]
+        details = row[fields["Details"]]
+        amount = ParseAmount(row[fields["Amount (INR)"]])
+        ref_no = row[fields["Reference Number"]]
+        print("%s, %s, %s, %s" % (date, details, amount, ref_no))
+        processed_lines.append((date, details, amount, ref_no))
+    return processed_lines
+
+def WriteOutput(lines, filename):
+    with open(filename, "w") as f:
+        writer = csv.writer(f)
+        writer.writerows(lines)
+    
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--input", help="File to parse")
+    parser.add_argument("-o", "--output", help="Location of output file")
+    args = parser.parse_args()
+    print("Input file: %s" % args.input)
+    print("output file: %s" % args.output)
+    processed_lines = processInputFile(args.input)
+    WriteOutput(processed_lines, args.output)
+
+main()
+    
+    
+        
+
+    
